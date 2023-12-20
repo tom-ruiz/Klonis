2c534ab6b489fe770975aa76ccd3042432fc4487
tom-ruiz <tomruiz2611@gmail.com>
2023-12-20 18:48:32 +0100

    bug fix presta

--- src/pages/pages/page-presta.js
+++ src/pages/pages/page-presta.js
@@ -12,12 +12,10 @@ import formation from "../../assets/presta/formation.png";
 import AOS from "aos";
 
 export default function PageInfo() {
-
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-
 
   const [isRotated, setIsRotated] = useState({});
 
@@ -33,13 +31,18 @@ export default function PageInfo() {
       <Navbar />
       <section className={styles.project}>
         <h3>Nos prestations</h3>
-        <div className={styles.wrapper} data-aos="fade-right" data-aos-duration="1000">
+        <div
+          className={styles.wrapper}
+          data-aos="fade-right"
+          data-aos-duration="1000"
+        >
           <div className={styles.card}>
             <Image src={devweb} alt="solution" className={styles.cardImg} />
             <div className={styles.description}>
               <h4>Développement de sites web</h4>
               <p>
-                Si vous souhaitez avoir un site vitrine, de gestion d'entreprise ou de réservations par exemple
+                Pour un site qui vous ressemble, qu'il s'agisse d'une vitrine,
+                d'un outil de gestion d'entreprise ou de réservations.{" "}
               </p>
             </div>
           </div>
@@ -48,7 +51,8 @@ export default function PageInfo() {
             <div className={styles.description}>
               <h4>Développement d'applications bureau</h4>
               <p>
-                Si vous optez pour une application de bureau personnelle ou d'entreprise avec des besoins spécifiques
+                Votre application sur mesure, que ce soit personnel ou
+                professionnel, répondant précisément à vos besoins spécifiques.
               </p>
             </div>
           </div>
@@ -57,7 +61,9 @@ export default function PageInfo() {
             <div className={styles.description}>
               <h4>Dépannage informatique</h4>
               <p>
-                Si vous voulez supprimer les virus de votre ordinateur, installer de nouveaux logiciels ou obtenir une assistance technique
+                Libérez votre ordinateur des virus, installez de nouveaux
+                logiciels, ou bénéficiez d'une assistance technique
+                personnalisée.{" "}
               </p>
             </div>
           </div>
@@ -66,7 +72,8 @@ export default function PageInfo() {
             <div className={styles.description}>
               <h4>Conseils informatiques</h4>
               <p>
-                Si vous envisagez d'acheter du matériel informatique que ce soit un PC bureautique ou gamer par exemple
+                Choisissez le matériel informatique idéal, que ce soit pour un
+                PC bureautique ou gamer, grâce à nos conseils.{" "}
               </p>
             </div>
           </div>
@@ -75,7 +82,8 @@ export default function PageInfo() {
             <div className={styles.description}>
               <h4>Expertise design applicatif</h4>
               <p>
-                Si vous réclamez une expertise de développement pour vos applications afin de monter en compétences en UX et UI
+                Demandez une expertise de design pour créer des maquettes et
+                offrir une expérience utilisateur (UX) concrète dans vos projets.{" "}
               </p>
             </div>
           </div>
@@ -84,7 +92,9 @@ export default function PageInfo() {
             <div className={styles.description}>
               <h4>Formations informatiques</h4>
               <p>
-                Si vous désirez des formations informatiques orientés sur Windows afin de prendre en main votre ordinateur et d'opter pour les bonnes pratiques afin d'éviter les virus
+                Prenez le contrôle avec nos formations orientées Windows et MacOS,
+                maîtrisez votre ordinateur et adoptez les meilleures pratiques
+                pour éviter les virus.{" "}
               </p>
             </div>
           </div>
