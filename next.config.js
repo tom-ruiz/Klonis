878f049a83ad7cb9be5d9ab69e634fa36da842b8
tom-ruiz <tomruiz2611@gmail.com>
2023-12-20 15:26:48 +0100

    modif config next et script

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 // next.config.js
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? '/klonis/' : '',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '',
+  assetPrefix: '',
 }
