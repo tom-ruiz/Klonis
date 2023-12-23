44c9675c1e680a0da8930c77031d38390ca1f7ba
Sniky <sniky83@hotmail.fr>
2023-12-23 11:58:00 +0100

    Dynamic Year footer

--- src/pages/component/Footer.js
+++ src/pages/component/Footer.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  
   return (
     <div className="footer">
       <Link href="mailto:klonispro@protonmail.com">
         klonispro@protonmail.com
       </Link>
-      © KLONIS 2023
+      © KLONIS {currentYear}
       <Link href="/pages/page-mention"> Mentions légales </Link>
     </div>
   );
